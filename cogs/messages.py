55195d0db549cb408d66cf1ddda49c55699e7a71
madison <69394836+xorsirenz@users.noreply.github.com>
2024-06-17 01:13:28 -0700

    purge limit

--- cogs/messages.py
+++ cogs/messages.py
@@ -4,7 +4,6 @@ import discord
 from discord.ext import commands
 from config import TOKEN
 
-token = TOKEN
 api_url = 'https://discord.com/api/v9/channels/'
 
 
@@ -18,17 +17,61 @@ class Messages(commands.Cog):
 
     @commands.command(brief='// <amount>')
     async def purge(self, ctx, amount=100):
-        if amount < 100:
+        if amount <= 100:
             await ctx.channel.purge(limit=amount, check=lambda message: message.author == ctx.author)
             return
         await ctx.message.edit(f'failed. 100 messages max.')
 
     @commands.command(brief='// <amount>')
     async def purgeall(self, ctx, amount:int):
-        if amount < 100:
+        if amount <= 100:
             await ctx.channel.purge(limit=amount)
             return
         await ctx.message.edit(f'failed. 100 messages max.')
+
+    @commands.command(brief='// <amount> <message>')
+    async def spam(self, ctx, times:int, *, message:str):
+        channel_id = ctx.channel.id
+
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for i in range(times):
+                tasks.append(asyncio.create_task(self.spamuwu(session, TOKEN, message, channel_id)))
+        
+            await asyncio.gather(*tasks)
+
+    async def spamuwu(self, session, TOKEN, message, channel_id):
+        headers = {
+            'Authorization': TOKEN,
+            'Content-Type': 'application/json'
+        }
+
+        spam_response = await session.post(f'{api_url}{channel_id}/messages',
+                        headers=headers,
+                        json={'content': message})
+
+    @commands.command(brief='// <amount> <user_id> <message>')
+    async def spamdm(self, ctx, times:int, user_id:int, *, message:str):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for i in range(times):
+                tasks.append(asyncio.create_task(self.spamdmuwu(session, TOKEN, user_id, message)))
+
+            await asyncio.gather(*tasks)
+
+    async def spamdmuwu(self, session, TOKEN, user_id, message):
+        headers = {
+                    'Authorization': TOKEN,
+                    'Content-Type': 'application/json'
+        }
+        dm_channel = await session.post('https://discord.com/api/v9/users/@me/channels',
+                                        headers=headers,
+                                        json={'recipient_id': user_id})
+        if dm_channel.status == 200:
+            dm_channel_id = (await dm_channel.json())['id']
+            dm_response = await session.post(f'{api_url}{dm_channel_id}/messages',
+                                        headers=headers,
+                                        json={'content': message})
 
 
 async def setup(bot):
