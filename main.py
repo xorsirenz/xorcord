e0eb6344583e97775a408414d8ce348ca1fe5e4f
madison <69394836+xorsirenz@users.noreply.github.com>
2024-06-17 01:12:34 -0700

    moved spam to cogs

--- main.py
+++ main.py
@@ -8,75 +8,28 @@ from discord.ext import commands
 from config import TOKEN
 
 bot = commands.Bot(command_prefix="-", self_bot=True)
-token = TOKEN
-api_url = 'https://discord.com/api/v9/channels/'
-
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+token = TOKEN
 
 @bot.event
 async def on_ready():
     print(f'lessgo', {bot.user.name}, {bot.user.id})
 
-@bot.command(brief='// <amount> <message>')
-async def spam(ctx, times:int, *, message:str):
-    channel_id = ctx.channel.id
-
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for i in range(times):
-            tasks.append(asyncio.create_task(spamuwu(session, token, message, channel_id)))
-        
-        await asyncio.gather(*tasks)
-
-async def spamuwu(session, token, message, channel_id):
-    headers = {
-        'Authorization': token,
-        'Content-Type': 'application/json'
-    }
-
-    spam_response = await session.post(f'{api_url}{channel_id}/messages',
-                        headers=headers,
-                        json={'content': message})
-
-@bot.command(brief='// <amount> <user_id> <message>')
-async def spamdm(ctx, times:int, user_id:int, *, message:str):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for i in range(times):
-            tasks.append(asyncio.create_task(spamdmuwu(session, token, user_id, message)))
-
-        await asyncio.gather(*tasks)
-
-async def spamdmuwu(session, token, user_id, message):
-    headers = {
-        'Authorization': token,
-        'Content-Type': 'application/json'
-    }
-    dm_channel = await session.post('https://discord.com/api/v9/users/@me/channels',
-                                    headers=headers,
-                                    json={'recipient_id': user_id})
-    if dm_channel.status == 200:
-        dm_channel_id = (await dm_channel.json())['id']
-        dm_response = await session.post(f'{api_url}{dm_channel_id}/messages',
-                                        headers=headers,
-                                        json={'content': message})
-
-
 @bot.command(brief='// <extension>')
 async def load(ctx, extension):
     await bot.load_extension(f'cogs.{extension}')
-    print(f"loaded extension {extension}")
-
-@bot.command(brief='// <extension>')
-async def unload(ctx, extension):
-    await bot.unload_extension(f'cogs.{extension}')
-    print(f"unloaded extension {extension}")
+    print(f"loaded extension: {extension}")
 
 @bot.command(brief='// <extension>')
 async def reload(ctx, extension):
     await bot.unload_extension(f'cogs.{extension}')
     await bot.load_extension(f'cogs.{extension}')
-    print(f"reloaded extension {extension}")
+    print(f"reloaded extension: {extension}")
+
+@bot.command(brief='// <extension>')
+async def unload(ctx, extension):
+    await bot.unload_extension(f'cogs.{extension}')
+    print(f"unloaded extension: {extension}")
 
 async def load_ext():
     for file in os.listdir('./cogs'):
@@ -88,7 +41,6 @@ async def main():
     async with bot:
         await load_ext()
         await bot.start(TOKEN)
-
 
 asyncio.run(main())
 
