20a7fabeeff0b44d631e03b3b4c1f0b432d26c66
madison <69394836+xorsirenz@users.noreply.github.com>
2024-08-13 18:38:49 -0700

    callback server

--- examples/example.config.py
+++ examples/example.config.py
@@ -1,12 +1,18 @@
 from urllib import parse
 
 API_URL = 'https://discord.com/api/v10/channels/'
+#selfbot
+TOKEN = '<SELFBOT_TOKEN>'
 CALLBACK_CHANNEL = '<PRIVATE_CHANNEL_ID>'
-
-TOKEN = '<SELF_TOKEN>'
-
+#oauthbot
 CLIENT_ID = "<BOT_ID>"
 BOT_TOKEN = "<BOT_TOKEN>"
 CLIENT_SECRET = "<CLIENT_SECRET>"
 REDIRECT_URI = "<http://localhost:5000/oauth/callback>"
 OAUTH_URL = f"https://discord.com/oauth2/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri{parse.quote(REDIRECT_URI)}&scope=identify+email+connections+guilds.join+guilds+messages.read"
+#c2 server
+ADMIN_TOKEN = '<ADMIN_TOKEN'
+GUILD_API = 'https://discord.com/api/v9/guilds'
+GUILD_ID = 'GUILD_ID'
+SELF_ID = 'SELF_ID'
+ROLE_ID = 'ROLE_ID'
