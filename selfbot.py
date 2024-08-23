a518d308798949e3e0e3b368f221fb7b1262756a
madison <69394836+xorsirenz@users.noreply.github.com>
2024-08-23 16:18:22 -0700

    populate config for token

--- selfbot.py
+++ selfbot.py
@@ -5,16 +5,13 @@ import logging
 import sys
 import discord
 from discord.ext import commands
-from config import TOKEN
-from utils import get_current_user
 
 bot = commands.Bot(command_prefix="-", self_bot=True)
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
-token = TOKEN
 
 @bot.event
 async def on_ready():
-    print(f'lessgo', {bot.user.name}, {bot.user.id})
+    print(f'logged in as: {bot.user.name}\ndiscord id: {bot.user.id}')
 
 @bot.command(brief='// <extension>')
 async def load(ctx, extension):
@@ -34,7 +31,7 @@ async def unload(ctx, extension):
 
 async def load_ext():
     for file in os.listdir('./cogs'):
-        if file.endswith('.py'):
+        if file.endswith('.py') and file != 'admin.py':
             await bot.load_extension(f'cogs.{file[:-3]}')
             print(f"loaded extension {file}")
 
@@ -45,4 +42,20 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        with open('config.py', 'r') as file:
+            file.close()
+    except FileNotFoundError:
+        print('no config file found')
+        user_token = input('Enter discord token:\n > ')
+        callback_channel = input('Enter private discord channel id:\n > ')
+        with open('config.py', 'a') as file:
+            file.write(f"from urllib import parse\nAPI_URL = 'https://discord.com/api/v10/channels/'\nTOKEN = '{user_token}'\nCALLBACK_CHANNEL = '{callback_channel}'")
+            file.close()
+        with open('raiders.txt', 'a') as f:
+            f.write(user_token)
+            f.close()
+    finally:
+        print('loading xorcord')      
+        from config import TOKEN
+        asyncio.run(main())
