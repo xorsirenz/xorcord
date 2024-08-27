ddbb77884e36a612313126ea76abad090d555119
madison <69394836+xorsirenz@users.noreply.github.com>
2024-08-27 05:58:17 -0700

    file structure

--- oauth/config.py
+++
@@ -1,8 +0,0 @@
-from urllib import parse
-#bot.py & webserver.py
-API_URL = 'https://discord.com/api/v10/'
-CLIENT_ID = "<BOT_ID>"
-BOT_TOKEN = "<BOT_TOKEN>"
-CLIENT_SECRET = "<CLIENT_SECRET>"
-REDIRECT_URI = "<http://localhost:5000/oauth/callback>"
-OAUTH_URL = f"https://discord.com/oauth2/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri{parse.quote(REDIRECT_URI)}&scope=identify+email+connections+guilds.join+guilds+messages.read"
