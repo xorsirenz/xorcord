e2440d2aa79d691adb20a5919ae723d6bab7818a
madison <69394836+xorsirenz@users.noreply.github.com>
2024-09-03 15:23:09 -0700

    token in function

--- src/cogs/roles.py
+++ src/cogs/roles.py
@@ -4,22 +4,24 @@ import discord
 from discord.ext import commands
 from selfbot import API_URL
 
-with open('config.json', 'r') as file:
-    config = json.load(file)
-    if 'admin_token' in config:
-        ADMIN_TOKEN = config.get('admin_token')
-    else:
-        answer = input('setup admin server yes or no? > ')
-        if answer.lower() == 'no':
-            pass
+def get_admin_token():
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+        if 'admin_token' in config:
+            global ADMIN_TOKEN
+            ADMIN_TOKEN = config.get('admin_token')
         else:
-            admin_config = {
-                'admin_token': input('enter guild user admin token: '),
-            }
-            ADMIN_TOKEN = admin_config.get('admin_token')
-            config.update(admin_config)
-            with open('config.json', 'w') as file:
-                json.dump(config, file, indent=4)
+            answer = input('setup admin server yes or no? > ')
+            if answer.lower() == 'no':
+                pass
+            else:
+                admin_config = {
+                        'admin_token': input('enter guild user admin token: '),
+                }
+                ADMIN_TOKEN = admin_config.get('admin_token')
+                config.update(admin_config)
+                with open('config.json', 'w') as file:
+                    json.dump(config, file, indent=4)
 
 class Roles(commands.Cog):
 
@@ -29,6 +31,7 @@ class Roles(commands.Cog):
     @commands.command(brief='// <role_id>', help='removes role')
     async def rmrole(self, ctx, role:str):
         try:
+            get_admin_token()
             headers = {'authorization': ADMIN_TOKEN}
             guild_id = ctx.guild.id
             self_id = self.bot.user.id
@@ -40,6 +43,7 @@ class Roles(commands.Cog):
     @commands.command(brief='// <role_id>', help='gives admin role (needs server admin token)')
     async def admin(self, ctx, role:str):
         try:
+            get_admin_token()
             headers = {'authorization': ADMIN_TOKEN}
             guild_id = ctx.guild.id
             self_id = self.bot.user.id
