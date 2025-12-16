2d74b0f1a1a7a0fa0ffcd0c30b311fff44c069c3
madison <69394836+xorsirenz@users.noreply.github.com>
2025-12-16 12:15:47 -0800

    formatted for mids backwards compatibility

--- src/cogs/atoh.py
+++ src/cogs/atoh.py
@@ -10,7 +10,7 @@ class AtoH(commands.Cog):
     async def atoh(self, ctx, message:str):
         hex_binary = message.encode(encoding='utf_8')
         hex_text = hex_binary.hex()
-        formatted_hex = " ".join(hex_text[i : i +2] for i in range(0, len(hex_text), 2))
+        formatted_hex = " ".join(hex_text[i : i +2] for i in range(0, len(hex_text), 2)).upper()
         await ctx.message.edit(formatted_hex)
 
 async def setup(bot):
